8c144393c00a3d747515460800f62895eb97cd11
Zachary Cohen <zacharycohen@Zacharys-MBP.home>
2020-04-28 09:27:25 -0400

    stonks working

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,8 @@
 const weatherBtn = document.getElementById("weatherButton");
+const stockBtn = document.getElementById('stockButton')
+
 const APIKey = "166a433c57516f51dfab1f7edaed8413";
+const stockApi= "TK1WYPTW0R3LJ9GP";
 
 weatherBtn.addEventListener("click", function () {
     var zip = document.getElementById("weatherInput").value;
@@ -23,4 +26,38 @@ weatherBtn.addEventListener("click", function () {
     });
 
 });
+
+stockBtn.addEventListener('click', function(){
+    var stock = document.getElementById("stockInput").value;
+    var queryURL = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol="+stock+"&apikey="+ stockApi
+    $('#stockInformation').empty()
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    }).then(function (response) {
+    // let myres = response['Meta Data']
+      console.log(response);
+     
+    var newLi = document.createElement("li");
+    var stockPrice = document.createElement('li');
+    var previousPrice = document.createElement('li');
+    var percentage = document.createElement('li');
+
+
+    newLi.innerHTML = ('Symbol: '+response['Global Quote']['01. symbol']);
+    stockPrice.innerHTML = ('price: ' +'$'+response['Global Quote']['05. price']);
+    previousPrice.innerHTML= ('Previous Close Price: '+ '$'+ response['Global Quote']['08. previous close'])
+    percentage.innerHTML = ('Change: ' + '$' + response['Global Quote']['09. change'])
+
+    document.getElementById("stockInformation").append(newLi);
+    document.getElementById("stockInformation").append(stockPrice);
+    document.getElementById('stockInformation').append(previousPrice);
+    document.getElementById('stockInformation').append(percentage);
+
+    // document.getElementById("weatherInformation").append(descriptionLi);
+ 
+    });
+
+    
+})
 
